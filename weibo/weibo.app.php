36c8d4f4d443fd87f6f7771ec8b5cdb225e32c1b
caopw84@gmail.com <caopuwei@fe.baidu.com>
2012-11-08 17:57:56 +0800

    by davidnk

--- weibo/weibo.app.php
+++ weibo/weibo.app.php
@@ -1,4 +1,19 @@
 <?php
-    
+   /**
+    *   weibo app class
+    */ 
 
+    class Demo{
+    
+        public function __construct(){
+        
+        }
+
+        public function excute(){
+           echo "excute\n; 
+        }
+    }
+
+    $app = new Demo();
+    $app->excute();
 ?>
