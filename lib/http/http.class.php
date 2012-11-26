bbe4a203685e1b7afa33308156afa015638411e4
caopw84@gmail.com <caopuwei@fe.baidu.com>
2012-11-26 19:14:22 +0800

    by davidnk

--- lib/http/http.class.php
+++ lib/http/http.class.php
@@ -19,8 +19,16 @@
             return $output;
         }
 
-        public function post( $url = "" ){
-            
+        public function post( $url = "" , $params ){
+            $this->ch = curl_init();
+            curl_setopt( $this->ch , CURLOPT_POST , 1);
+            curl_setopt( $this->ch , CURLOPT_URL , $url);
+            curl_setopt( $this->ch , CURLOPT_POSTFIELDS , $params );
+            ob_start();
+            curl_exec( $this->ch );
+            $result = ob_get_contents();
+            ob_end_clean();
+            echo $result;
         }
     
     }
--- lib/http/http.test.php
+++ lib/http/http.test.php
@@ -3,5 +3,11 @@
     require_once("http.class.php");
 
     $hp = new HttpHelper();
-    echo $hp->get( "http://www.baidu.com" );
+    $data = array(
+        'name=caopuwei',
+        'passwd=1234567'
+    );
+    $postData = implode('&',$data);
+    $hp->post("http://fe.baidu.com/~caopuwei/php/lib/http/post.test.php" , $postData);
+    //echo $hp->get( "http://www.baidu.com" );
 ?>
