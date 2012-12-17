cbe0555f6f1ffb2fe90603f37aa9d7dca6cdf538
caopw84@gmail.com <caopuwei@fe.baidu.com>
2012-12-17 11:22:26 +0800

    by davidnk

--- app/weibo/sdk/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-
-define( "WB_AKEY" , '2248720749' );
-define( "WB_SKEY" , '2ef0ee49b9fa41ff314b0d9c839a22ac' );
-define( "WB_CALLBACK_URL" , 'http://fe/~caopuwei/php/weibo/callback.php' );
